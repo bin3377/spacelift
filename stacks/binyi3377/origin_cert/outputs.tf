3f81f4932ccee196397f3a549817f4fce54bcc62
Bin Yi <binyi3377@gmail.com>
2025-06-09 09:31:19 -0700

    test remove sensitive

--- stacks/binyi3377/origin_cert/outputs.tf
+++ stacks/binyi3377/origin_cert/outputs.tf
@@ -1,9 +1,7 @@
 output "certificate" {
   value     = module.origin_cert.certificate
-  sensitive = true
 }
 
 output "private_key" {
   value     = module.origin_cert.private_key
-  sensitive = true
 }
