9f924bc210332becdc47ff172ab4e2e85247a9f2
Bin Yi <binyi3377@gmail.com>
2025-06-09 10:05:07 -0700

    variables.tf

--- stacks/sanjose/ingress/variables.tf
+++ stacks/sanjose/ingress/variables.tf
@@ -1,3 +1,28 @@
+variable "private_key_path" {
+  type        = string
+  description = "the path to the private key of oci-cli"
+}
+
+variable "fingerprint" {
+  type        = string
+  description = "the fingerprint of oci-cli"
+}
+
+variable "user_ocid" {
+  type        = string
+  description = "the user ocid of oci-cli"
+}
+
+variable "tenancy_ocid" {
+  type        = string
+  description = "the tenancy ocid of oci-cli"
+}
+
+variable "compartment_ocid" {
+  type        = string
+  description = "the compartment ocid of oci-cli"
+}
+
 variable "cluster_ocid" {
   type        = string
   description = "the cluster ocid"
