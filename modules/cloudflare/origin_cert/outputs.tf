524886f099529a9e03bbf7508719fd58b7299572
Bin Yi <binyi3377@gmail.com>
2025-06-09 09:32:54 -0700

    remove sensitive

--- modules/cloudflare/origin_cert/outputs.tf
+++ modules/cloudflare/origin_cert/outputs.tf
@@ -1,9 +1,7 @@
 output "certificate" {
   value     = cloudflare_origin_ca_certificate.this.certificate
-  sensitive = true
 }
 
 output "private_key" {
   value     = tls_private_key.this.private_key_pem
-  sensitive = true
 }
