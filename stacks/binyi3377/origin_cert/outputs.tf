5a64954f54087308a8c582ee10b903322ce6d80f
Bin Yi <binyi3377@gmail.com>
2025-06-09 09:34:37 -0700

    revert

--- stacks/binyi3377/origin_cert/outputs.tf
+++ stacks/binyi3377/origin_cert/outputs.tf
@@ -1,7 +1,9 @@
 output "certificate" {
   value     = module.origin_cert.certificate
+  sensitive = true
 }
 
 output "private_key" {
   value     = module.origin_cert.private_key
+  sensitive = true
 }
