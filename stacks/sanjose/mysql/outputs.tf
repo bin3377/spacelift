21139281887d234ccd5d9d1af13c4531d0031a5e
Bin Yi <binyi3377@gmail.com>
2025-06-09 13:53:02 -0700

    output user/pass

--- stacks/sanjose/mysql/outputs.tf
+++ stacks/sanjose/mysql/outputs.tf
@@ -6,3 +6,12 @@ output "db_ip" {
 output "db_port" {
   value = module.mysql.port
 }
+
+output "db_username" {
+  value = var.admin_username
+}
+
+output "db_password" {
+  value     = var.admin_password
+  sensitive = true 
+}
