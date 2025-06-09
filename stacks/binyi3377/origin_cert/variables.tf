374839614d46e05a672612b3f03a301c5d484014
Bin Yi <binyi3377@gmail.com>
2025-06-09 00:32:21 -0700

    origin_cert

---
+++ stacks/binyi3377/origin_cert/variables.tf
@@ -0,0 +1,4 @@
+variable "dns_names" {
+  type        = list(string)
+  description = "the dns names to create the origin ca certificate for"
+}
--- stacks/sanjose/k8s/main.tf
+++ stacks/sanjose/k8s/main.tf
@@ -3,16 +3,16 @@ module "cluster" {
   compartment_id     = var.compartment_ocid
   kubernetes_version = "v1.33.0"
   prefix             = "k8s"
-  node_shape_config  = {
+  node_shape_config = {
     memory_in_gbs = 12
     ocpus         = 2
   }
-  vcn_id             = var.vcn_ocid
-  private_subnet_id  = var.private_subnet_ocid
-  public_subnet_id   = var.public_subnet_ocid
-  ssh_public_key     = var.ssh_public_key
-  enable_dashboard   = false
-  enable_tiller      = true
+  vcn_id            = var.vcn_ocid
+  private_subnet_id = var.private_subnet_ocid
+  public_subnet_id  = var.public_subnet_ocid
+  ssh_public_key    = var.ssh_public_key
+  enable_dashboard  = false
+  enable_tiller     = true
 
   defined_tags  = var.defined_tags
   freeform_tags = var.freeform_tags
