830f28201ede9da53c0404eaa75826a4a1677cf8
Bin Yi <binyi3377@gmail.com>
2025-06-09 10:01:27 -0700

    add auth vars

--- stacks/sanjose/ingress/main.tf
+++ stacks/sanjose/ingress/main.tf
@@ -13,8 +13,29 @@ provider "kubernetes" {
 
   exec {
     api_version = local.kubeconfig_user_exec["apiVersion"]
-    args        = local.kubeconfig_user_exec["args"]
+    args        = concat(
+      local.kubeconfig_user_exec["args"],
+      ["--compartment-id", var.compartment_ocid]
+    )
     command     = local.kubeconfig_user_exec["command"]
+    env         = [
+      {
+        name  = "OCI_CLI_USER"
+        value = var.user_ocid
+      },
+      {
+        name  = "OCI_CLI_FINGERPRINT"
+        value = var.fingerprint
+      },
+      {
+        name  = "OCI_CLI_TENANCY"
+        value = var.tenancy_ocid
+      },
+      {
+        name  = "OCI_CLI_KEY_FILE"
+        value = var.private_key_path
+      }
+    ]
   }
 }
 
